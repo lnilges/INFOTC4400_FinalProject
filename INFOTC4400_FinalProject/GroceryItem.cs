8254ec867557270aa5c72509bebfc360c6ef6412
Cable, Sydney (MU-Student) <smcndt@umsystem.edu>
2026-04-28 20:12:58 -0500

    changed the xaml layout

--- INFOTC4400_FinalProject/GroceryItem.cs
+++ INFOTC4400_FinalProject/GroceryItem.cs
@@ -1,9 +1,18 @@
 ﻿using System;
 
 //GroceryItem - contains all information from ingredient + quantity to purchase 
+//Sydney work on this
 public class GroceryItem : Ingredient
 {
+	bool isBought = false;
+	string itemName;
+	int itemQuantity;
+
 	public GroceryItem()
 	{
+		//I want to add items like ingredients to the grocerylist and then pull the list into the listbox
+		List <string> GroceryList = new List<string> ();
+
+
 	}
 }
